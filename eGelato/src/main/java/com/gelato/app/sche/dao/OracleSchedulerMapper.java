705ab4bf6396f63936a56c3c3df552af4b5f7be0
rp177 <rphenomenon177@gmail.com>
2022-01-30 20:16:11 +0900

    머지준비 - 글로벌 변경

--- eGelato/src/main/java/com/gelato/app/sche/dao/OracleSchedulerMapper.java
+++ eGelato/src/main/java/com/gelato/app/sche/dao/OracleSchedulerMapper.java
@@ -2,4 +2,8 @@ package com.gelato.app.sche.dao;
 
 public interface OracleSchedulerMapper {
 
+	void mkJob(OracleSchedulerVO vo);
+	void startJob(OracleSchedulerVO vo);
+	void stopJob(OracleSchedulerVO vo);
+	
 }
